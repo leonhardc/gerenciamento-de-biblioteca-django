679bb26c1bfe9e23132851f1932d3a34e0959602
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-01 17:43:25 -0300

    Adicionado model dos dois primeiros apps do projeto autor/livro

--- autor/models.py
+++ autor/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Autor(models.Model):
+    nome = models.CharField(max_length=200, blank=False, null=False, verbose_name='Nome')
+    cpf = models.CharField(max_length=11, blank=False, null=False, verbose_name='CPF')
+    nacionalidade = models.CharField(max_length=50, blank=False, null=False, verbose_name='Nacionalidade')
--- livro/models.py
+++ livro/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+
+# Model de app livro
+
+class Livro(models.Model):
+    isbn = models.CharField(max_length=13, blank=False, null=False, verbose_name='ISBN')
+    titulo = models.CharField(max_length=200, blank=False, null=False, verbose_name='Titulo')
+    ano_lancamento = models.CharField(max_length=4, blank=False, null=False, verbose_name='Ano de Lançamento')
+    qt_copias = models.IntegerField(blank=False, null=False, verbose_name='Quantidade de Cópias')
+    # TODO: Adicionar o campo de Autores
+
