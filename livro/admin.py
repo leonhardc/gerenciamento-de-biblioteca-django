f018c4cd12e16405b6b6ab458fdb3d7ea7421f94
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-02 08:57:50 -0300

    configuração dos models do app livro e registrado app na tela administrativa

--- livro/admin.py
+++ livro/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Livro, Categoria
 
-# Register your models here.
+class LivroAdmin(admin.ModelAdmin):
+    list_display = [
+        'isbn',
+        'titulo',
+        'qt_copias',
+        'autores',
+    ]
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = [
+        'cod_categoria',
+        'nome_categoria',
+        'desc_categoria',
+    ]
+
+admin.site.register(Livro, LivroAdmin)
+admin.site.register(Categoria, CategoriaAdmin)
--- livro/models.py
+++ livro/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from autor.models import Autor
 
 
 # Model de app livro
@@ -10,5 +11,9 @@ class Livro(models.Model):
     titulo = models.CharField(max_length=200, blank=False, null=False, verbose_name='Titulo')
     ano_lancamento = models.CharField(max_length=4, blank=False, null=False, verbose_name='Ano de Lançamento')
     qt_copias = models.IntegerField(blank=False, null=False, verbose_name='Quantidade de Cópias')
-    # TODO: Adicionar o campo de Autores
+    autores = models.ManyToManyField(Autor, verbose_name='Autores')
 
+class Categoria(models.Model):
+    cod_categoria = models.CharField(max_length=4, blank=False, null=False, verbose_name='Código da Categoria')
+    nome_categoria = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Categoria')
+    desc_categoria = models.CharField(max_length=500, blank=True, null=True, verbose_name='Descrição')
