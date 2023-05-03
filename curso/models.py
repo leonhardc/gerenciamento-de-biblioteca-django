f6e906c3f4c85201540e968ff703b623ef87b72b
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-03 20:18:25 -0300

    Adicionada classe Meta e o método __str__ ao model dos apps curso e livro

--- curso/models.py
+++ curso/models.py
@@ -3,3 +3,10 @@ from django.db import models
 class Curso(models.Model):
     cod_curso = models.CharField(max_length=3, blank=False, verbose_name='Codigo do Curso')
     nome_curso = models.CharField(max_length=50, blank=False, verbose_name='Nome do Curso')
+
+    def __str__(self) -> str:
+        return f'{self.cod_curso} - ({self.nome_curso})'
+    
+    class Meta:
+        verbose_name = 'Curso'
+        verbose_name_plural = 'Cursos'
--- livro/models.py
+++ livro/models.py
@@ -1,13 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
-from autor.models import Autor
 
+class Autor(models.Model):
+    nome = models.CharField(max_length=200, blank=False, verbose_name='Nome')
+    cpf = models.CharField(max_length=11, blank=False, unique=True, verbose_name='CPF')
+    nacionalidade = models.CharField(max_length=50, blank=False, verbose_name='Nacionalidade')
+
+    def __str__(self) -> str:
+        return f'{self.nome}, {self.nacionalidade}'
+
+    class Meta:
+        verbose_name = 'Autor'
+        verbose_name_plural = 'Autores'
 
 class Categoria(models.Model):
     cod_categoria = models.CharField(max_length=4, blank=False, unique=True, verbose_name='Código da Categoria')
     nome_categoria = models.CharField(max_length=100, blank=False, null=False, verbose_name='Nome da Categoria')
     desc_categoria = models.TextField(max_length=500, blank=True, null=True, verbose_name='Descrição')
+
+    def __str__(self) -> str:
+        return f'{self.cod_categoria} - {self.nome_categoria}'
+
+    class Meta:
+        verbose_name = 'Categoria'
+        verbose_name_plural = 'Categorias'
 
 # Model de app livro
 class Livro(models.Model):
@@ -17,4 +34,11 @@ class Livro(models.Model):
     qt_copias = models.IntegerField(blank=False, null=False, verbose_name='Quantidade de Cópias')
     autores = models.ManyToManyField(Autor, verbose_name='Autores')
     categoria = models.ManyToManyField(Categoria, verbose_name='Categoria')
+
+    def __str__(self) -> str:
+        return f'{self.titulo}'
+
+    class Meta:
+        verbose_name = 'Livro'
+        verbose_name_plural = 'Livros'
 
