1d7c34faf0117c32d0e29c7ef2042294e0bb15f2
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-03 20:16:16 -0300

    Project update - Exclusão do app autor e seu model foi vinculado ao app livro

--- autor/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Autor(models.Model):
-    nome = models.CharField(max_length=200, blank=False, verbose_name='Nome')
-    cpf = models.CharField(max_length=11, blank=False, unique=True, verbose_name='CPF')
-    nacionalidade = models.CharField(max_length=50, blank=False, verbose_name='Nacionalidade')
