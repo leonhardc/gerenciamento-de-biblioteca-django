7ca403f3b81d228f6340abce3bd35b70687a8436
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-15 19:57:30 -0300

    adicionado template e trecho de código do caso de pagenotfound

--- usuario/views.py
+++ usuario/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
 from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 from .models import Aluno, Professor, Funcionario, Endereco
+from django.http import Http404
 
 # Create your views here.
 
@@ -13,7 +15,9 @@ ALUNO_INDEX = 'aluno_index.html'
 PROFESSOR_INDEX = 'professor_index.html'
 FUNCIONARIO_INDEX = 'funcionario_index.html'
 ADMIN_INDEX = 'admin_index.html'
+NOT_FOUND_404_INDEX = '404.html'
 # contantes
+
 
 def index(request):
     return render(request, INDEX)
@@ -21,54 +25,73 @@ def index(request):
 def loginPage(request):
     return render(request, LOGIN)
 
+@login_required(redirect_field_name='login')
 def bibliotecaAluno(request):
-    return render(request, ALUNO_INDEX)
+    if request.method == 'GET':
+        isAluno = Aluno.objects.filter(usuario__username = request.user.username)
+        if isAluno:
+            return render(request, ALUNO_INDEX)
+        else:
+            return render(request, NOT_FOUND_404_INDEX)
 
+@login_required(redirect_field_name='login')
 def bibliotecaProfessor(request):
-    return render(request, PROFESSOR_INDEX)
+    if request.method == 'GET':
+        isProfessor = Professor.objects.filter(usuario__username = request.user.username)
+        if isProfessor:
+            return render(request, PROFESSOR_INDEX)
+        else:
+            return render(request, NOT_FOUND_404_INDEX)
 
+@login_required(redirect_field_name='login')
 def bibliotecaFuncionario(request):
-    return render(request, FUNCIONARIO_INDEX)
+    if request.method == 'GET':
+        isFuncionario = Funcionario.objects.filter(usuario__username = request.user.username)
+        if isFuncionario:
+            return render(request, FUNCIONARIO_INDEX)
+        else:
+            return render(request, NOT_FOUND_404_INDEX)
 
+
+@login_required(redirect_field_name='login')
 def bibliotecaAdmin(request):
     return render(request, ADMIN_INDEX)
 
 def login(request):
+    
+    if request.method == 'POST':
+        usuario = request.POST.get('usuario')
+        senha = request.POST.get('senha')
 
-    if request.method != 'POST':
-        # Adicionar mensagem de erro
+        user = authenticate(
+            username = usuario,
+            password = senha
+        )
+
+        if user:
+            # Verificar se o usuário é um aluno
+            aluno = Aluno.objects.filter(usuario__username = usuario) 
+            if aluno:
+                auth.login(request, user)
+                return redirect('/biblioteca-aluno/')
+
+            # Verificar se o usuário é um professor
+            professor = Professor.objects.filter(usuario__username = usuario)
+            if professor:
+                auth.login(request, user)
+                return redirect('/biblioteca-professor/')
+
+            # Verificar de o usuário é um funcionário
+            funcionario = Funcionario.objects.filter(usuario__username = usuario)
+            if funcionario:
+                auth.login(request, user)
+                return redirect('/biblioteca-funcionario/')
+                # Verificar se o usuário é um administrador, se sim, logar na ppagina de admin
+                # se não, logar na página de funcionário comum
+            pass
+        else:
+            # Retornar uma mensagem de erro
+            pass
+    
+    else: 
         return render(request, LOGIN)
-
-    usuario = request.POST.get('usuario')
-    senha = request.POST.get('senha')
-
-    user = authenticate(
-        username = usuario,
-        password = senha
-    )
-
-    if user:
-        # Verificar se o usuário é um aluno
-
-        aluno = Aluno.objects.filter(usuario__username = usuario) 
-        if aluno:
-            auth.login(request, usuario)
-            return redirect('/biblioteca-aluno/')
-
-        # Verificar se o usuário é um professor
-        professor = Professor.objects.filter(usuario__username = usuario)
-        if professor:
-            auth.login(request, usuario)
-            return redirect('/biblioteca-professor/')
-
-        # Verificar de o usuário é um funcionário
-        funcionario = Funcionario.objects.filter(usuario__username = usuario)
-        if funcionario:
-            auth.login(request, usuario)
-            return redirect('/biblioteca-funcionario/')
-            # Verificar se o usuário é um administrador, se sim, logar na ppagina de admin
-            # se não, logar na página de funcionário comum
-        pass
-    else:
-        # Retornar uma mensagem de erro
-        pass
