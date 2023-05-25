4f972cce5f812aede1139fbec980a4afbc9f8cbf
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-25 08:58:00 -0300

    Implementada view de listar livros e inserido a declaração da view de buscar livros

--- livro/views.py
+++ livro/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Autor, Categoria, Livro
 
-# Create your views here.
+
+LIVROS = 'livros.html'
+
+def busca_livros(request):
+    if request.method == 'GET':
+        pass
+
+def lista_livros(request):
+    if request.method == 'GET':
+        livros = Livro.objects.all()
+        return render(
+            request,
+            template_name=LIVROS,
+            context={'livros': livros}
+        )
