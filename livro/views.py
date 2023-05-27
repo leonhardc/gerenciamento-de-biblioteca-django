0887021baf1b3b40e71a6e033a0d9d430a868c37
Leonardo Rodrigues <leonardo99692370costa@gmail.com>
2023-05-27 17:58:29 -0300

    Adicionado declaração das views do app livro

--- livro/views.py
+++ livro/views.py
@@ -4,10 +4,6 @@ from .models import Autor, Categoria, Livro
 
 LIVROS = 'livros.html'
 
-def busca_livros(request):
-    if request.method == 'GET':
-        pass
-
 def lista_livros(request):
     if request.method == 'GET':
         livros = Livro.objects.all()
@@ -16,3 +12,52 @@ def lista_livros(request):
             template_name=LIVROS,
             context={'livros': livros}
         )
+
+# TODO; Implementar view para buscar livros
+def busca_livros(request):
+    if request.method == 'GET':
+        pass
+
+# TODO: Implementar view de inserir livros
+def insere_livro(request):
+    if request.method == 'POST':
+        ... 
+
+# TODO: Implementar view de excluir livro
+def excluir_livro(request):
+    ...
+
+# TODO: Implementar view de listar autores
+def lista_autores(request):
+    ...
+
+# TODO> Implementar view de inserir Autor
+def inserir_autor(request):
+    if request.method == 'POST':
+        ...
+
+# TODO: Implementar view de excluir autor
+def excluir_autor(request):
+    ...
+
+# TODO: Implementar view que lista categorias
+def lista_categorias(request):
+    ...
+
+# TODO: Inserir view de inserir categorias
+def inserir_categoria(request):
+    if request.method == 'POST':
+        ...
+
+#  TODO: Inserir view de excluir categorias
+def excluir_categoria(request):
+    ...
+
+# TODO: Implementar view para realizar uma reserva
+def fazer_reserva(request):
+    if request.method == 'POST':
+        ...
+
+# TODO: Implementar view para excluir reserva
+def excluir_reserva(request):
+    ...
